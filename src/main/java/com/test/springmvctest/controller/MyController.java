86edde5b58353608ac882c9d030d59a8be6c82e0
root <2898116867@qq.com>
2019-12-11 17:13:47 +0800

    hello word two

--- src/main/java/com/test/springmvctest/controller/MyController.java
+++ src/main/java/com/test/springmvctest/controller/MyController.java
@@ -9,6 +9,7 @@ public class MyController {
     @RequestMapping("/helloword")
     public String HelloController(){
         System.out.println("hello world");
+//        经过视图解析器的prefix前缀+返回值+suffix后缀转发到形视图，/WEB-INF/view/+success+.jsp
         return "success";
     }
 }
